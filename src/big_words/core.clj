7a5212a5ae782973badc4b411d6f74a2f3115250
Erin <emb255@nau.edu>
2017-10-01 14:22:59 -0500

    HELLO WORLD ON A SPECIFIED PORT

--- src/big_words/core.clj
+++ src/big_words/core.clj
@@ -1,6 +1,14 @@
 (ns big-words.core
-  (:gen-class))
+  (:gen-class)
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.adapter.jetty :refer [run-jetty]]))
+
+(defroutes app
+  (GET "/" [] "<h1>Hello, World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (run-jetty app {:port (Integer/parseInt (System/getenv "PORT"))}))
